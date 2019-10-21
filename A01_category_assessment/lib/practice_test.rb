eaffeb3626047f0c82f1ff43313f74a1e073cd6f
tombetthauser <tombetthauser@gmail.com>
2019-10-21 01:43:24 -0700

    Worked on solving my_join method

--- A01_category_assessment/lib/practice_test.rb
+++ A01_category_assessment/lib/practice_test.rb
@@ -751,7 +751,41 @@ class Array
   end
 end
 
-# 8min(rusty, hash syntax), 8min(rusty on hash sytax)
+# 8min(rusty, hash syntax), 8min(rusty on hash sytax), 4min
+
+
+
+
+
+
+
+
+
+class Array
+  # Write an `Array#my_join` method. If my_join receives no argument, then use
+  # an empty string to join the array together.
+  # **Do NOT use the built-in `Array#join` method in your implementation.**
+  #
+  # Examples.
+  # `[1,2,3].my_join` => '123'
+  # `[1,2,3].my_join('$')` => '1$2$3'
+
+  # debugger
+  def my_join(separator = "")
+    joined_str = ""
+    i = 0
+    self.each do |el|
+      joined_str << el.to_s
+      joined_str << separator unless i == self.length - 1 
+      i += 1
+    end
+
+    joined_str
+  end
+end
+
+
+
 
 
 
@@ -832,19 +866,6 @@ class Hash
   end
 end
 
-class Array
-  # Write an `Array#my_join` method. If my_join receives no argument, then use
-  # an empty string to join the array together.
-  # **Do NOT use the built-in `Array#join` method in your implementation.**
-  #
-  # Examples.
-  # `[1,2,3].my_join` => '123'
-  # `[1,2,3].my_join('$')` => '1$2$3'
-
-  def my_join(separator = "")
-
-  end
-end
 
 class Array
   # Write an `Array#my_each_with_index(&prc)` method that calls a proc on each 
