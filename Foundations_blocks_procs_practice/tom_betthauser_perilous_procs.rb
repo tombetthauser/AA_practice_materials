0db5b09a567e37460eb5beb3272ec74a8b24d8da
tombetthauser <tombetthauser@gmail.com>
2019-10-22 00:26:42 -0700

    Continued working on perilous_procs methods

--- Foundations_blocks_procs_practice/tom_betthauser_perilous_procs.rb
+++ Foundations_blocks_procs_practice/tom_betthauser_perilous_procs.rb
@@ -351,30 +351,47 @@ end
 
 #     For example, if three procs are passed in, then the hash should have the keys 1, 2, and 3.
 
-# The values associated with each key should be an array containing the elements of the input array that return true when passed into the corresponding proc.
+# The values associated with each key should be an array containing the elements 
+# of the input array that return true when passed into the corresponding proc.
 
-#     For example, this means that the array that corresponds to the key 2 should contain the elements that return true when passed into the second proc.
+#     For example, this means that the array that corresponds to the key 2 should 
+# contain the elements that return true when passed into the second proc.
 
-# If an element returns true for multiple procs, then it should only be placed into the array that corresponds to the proc that appears first in the arguments.
+# If an element returns true for multiple procs, then it should only be placed into 
+# the array that corresponds to the proc that appears first in the arguments.
+
+def proctition_platinum(arr, *procs)
+  hash = Hash.new #{ |h,k| h[k] = Array.new }
+  arr.length.times { |num| hash[num] = Array.new }
+  i = 1
+  procs.each do |prc|
+    arr.each do |ele| 
+      hash[i] << prc.call(ele)
+    end
+    i += 1
+  end
+  hash
+end
+
 
 # Examples
 
-# is_yelled = Proc.new { |s| s[-1] == '!' }
-# is_upcase = Proc.new { |s| s.upcase == s }
-# contains_a = Proc.new { |s| s.downcase.include?('a') }
-# begins_w = Proc.new { |s| s.downcase[0] == 'w' }
+is_yelled = Proc.new { |s| s[-1] == '!' }
+is_upcase = Proc.new { |s| s.upcase == s }
+contains_a = Proc.new { |s| s.downcase.include?('a') }
+begins_w = Proc.new { |s| s.downcase[0] == 'w' }
 
-# p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, contains_a)
-# # {1=>["when!", "WHERE!"], 2=>["what"]}
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["what"]}
 
-# p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, is_upcase, contains_a)
-# # {1=>["when!", "WHERE!"], 2=>["WHO", "WHY"], 3=>["what"]}
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_yelled, is_upcase, contains_a)
+# {1=>["when!", "WHERE!"], 2=>["WHO", "WHY"], 3=>["what"]}
 
-# p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_upcase, is_yelled, contains_a)
-# # {1=>["WHO", "WHERE!", "WHY"], 2=>["when!"], 3=>["what"]}
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "WHERE!", "WHY"], 2=>["when!"], 3=>["what"]}
 
-# p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
-# # {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]}
+p proctition_platinum(['WHO', 'what', 'when!', 'WHERE!', 'how', 'WHY'], begins_w, is_upcase, is_yelled, contains_a)
+# {1=>["WHO", "what", "when!", "WHERE!", "WHY"], 2=>[], 3=>[], 4=>[]}
 
 # procipher
 
@@ -448,38 +465,38 @@ end
 
 # Examples
 
-is_yelled = Proc.new { |s| s[-1] == '!' }
-is_upcase = Proc.new { |s| s.upcase == s }
-contains_a = Proc.new { |s| s.downcase.include?('a') }
-make_question = Proc.new { |s| s + '???' }
-reverse = Proc.new { |s| s.reverse }
-add_smile = Proc.new { |s| s + ':)' }
+# is_yelled = Proc.new { |s| s[-1] == '!' }
+# is_upcase = Proc.new { |s| s.upcase == s }
+# contains_a = Proc.new { |s| s.downcase.include?('a') }
+# make_question = Proc.new { |s| s + '???' }
+# reverse = Proc.new { |s| s.reverse }
+# add_smile = Proc.new { |s| s + ':)' }
 
-p picky_procipher('he said what!',
-    is_yelled => make_question,
-    contains_a => reverse
-) # "he dias what!???"
+# p picky_procipher('he said what!',
+#     is_yelled => make_question,
+#     contains_a => reverse
+# ) # "he dias what!???"
 
-p picky_procipher('he said what!',
-    contains_a => reverse,
-    is_yelled => make_question
-) # "he dias !tahw"
+# p picky_procipher('he said what!',
+#     contains_a => reverse,
+#     is_yelled => make_question
+# ) # "he dias !tahw"
 
-p picky_procipher('he said what!',
-    contains_a => reverse,
-    is_yelled => add_smile
-) # "he dias !tahw"
+# p picky_procipher('he said what!',
+#     contains_a => reverse,
+#     is_yelled => add_smile
+# ) # "he dias !tahw"
 
-p picky_procipher('stop that taxi now',
-    is_upcase => add_smile,
-    is_yelled => reverse,
-    contains_a => make_question
-) # "stop that??? taxi??? now"
+# p picky_procipher('stop that taxi now',
+#     is_upcase => add_smile,
+#     is_yelled => reverse,
+#     contains_a => make_question
+# ) # "stop that??? taxi??? now"
 
-p picky_procipher('STOP that taxi!',
-    is_upcase => add_smile,
-    is_yelled => reverse,
-    contains_a => make_question
-) # "STOP:) that??? !ixat"
+# p picky_procipher('STOP that taxi!',
+#     is_upcase => add_smile,
+#     is_yelled => reverse,
+#     contains_a => make_question
+# ) # "STOP:) that??? !ixat"
 
 # † pun intended
