63d66a21413785a996ac1902ceee2dc9c033b6d0
tombetthauser <tombetthauser@gmail.com>
2019-10-22 18:10:47 -0700

    Completed dog_project in classes_practice

--- Foundations_classes_practice/tom_betthauser_dog_project/lib/dog.rb
+++ Foundations_classes_practice/tom_betthauser_dog_project/lib/dog.rb
@@ -1,3 +1,25 @@
 class Dog
 
+  attr_reader :name, :breed, :bark, :favorite_foods, :age
+
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+  def age=(num)
+    @age = num
+  end
+
+  def bark
+    @age > 3 ? @bark.upcase : @bark.downcase
+  end
+
+  def favorite_food?(food)
+    @favorite_foods.map(&:downcase).include?(food.downcase)
+  end
+
 end
