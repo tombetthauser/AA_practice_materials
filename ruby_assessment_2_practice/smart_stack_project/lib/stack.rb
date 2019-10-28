55420623a76a5f0ed3de59455e00c1bf22caf279
tombetthauser <tombetthauser@gmail.com>
2019-10-28 15:01:14 -0700

    Added peek method to Stack class in A02 practice directory

--- ruby_assessment_2_practice/smart_stack_project/lib/stack.rb
+++ ruby_assessment_2_practice/smart_stack_project/lib/stack.rb
@@ -17,5 +17,18 @@ class Stack
     @underlying_array[-1]
   end
 
+  def peek(n = nil)
+    n ||= 1
+
+    if n >= @underlying_array.length
+      popped = @underlying_array
+      nils_arr = Array.new(n - @underlying_array.length, nil) 
+      return popped.reverse + nils_arr
+    end
+
+    n *= -1
+    popped = @underlying_array[n..-1]
+    popped.reverse
+  end
 
 end
